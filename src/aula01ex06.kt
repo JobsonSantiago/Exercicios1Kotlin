2d7ba94c67479b784a5ec848b62f86a365831752
JobsonSantiago <jobson.santiago@etec.sp.gov.br>
2024-02-26 18:39:39 -0300

    Lista de Exercicios Kotlin

---
+++ src/aula01ex06.kt
@@ -0,0 +1,6 @@
+package fundamentos
+
+fun main(args:Array<String>){
+    val aprovado = listOf("João", "Maria", "Pedro")
+    print("O primeiro colocado foi ${aprovado[0]}")
+}
---
+++ src/aula01ex14.kt
@@ -0,0 +1,11 @@
+package fundamentos.controles
+
+fun main(args: Array<String>){
+    var opcao: Int = 0
+    while(opcao != -1){
+      val line = readLine() ?: "0"
+      opcao = line.toIntOrNull() ?: 0
+      println("Você escolheu a opcao $opcao")
+    }
+    println("Até a próxima!")
+}
